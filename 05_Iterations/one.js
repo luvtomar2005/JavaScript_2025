a4bfbaf9b7a95aea30cd5abe081cfbade7fcf35f
luvtomar2005 <luvt354@gmail.com>
2025-09-04 08:27:28 +0530

    Added

---
+++ 05_Iterations/one.js
@@ -0,0 +1,42 @@
+// // for loop
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    console.log(index)
+    
+}
+
+
+for (let index = 1; index <= 20; index++) {
+    console.log(`Outer loop value : ${index}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(`Inner loop value ${j} and inner loop ${index}`);
+        console.log(index + '*' + j + ' = ' + index*j);
+        
+    }
+    
+}
+
+let myArray = ["flash" , "batman" , "superman"]
+console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element)
+}
+
+// break and continue
+for (let index = 1; index <= 20; index++) {
+    if(index == 5){
+        console.log(`Detected 5`);
+        break
+    }
+    console.log(`Value of i is ${index}`);
+    
+}
+
+for(let index = 1; index <= 20;index++){
+    if(index == 5){
+        console.log(`Detected 5`)
+        continue
+    }
+    console.log(`Value of i is ${index}`);
+}
